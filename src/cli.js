17dcaa12057bd3c4f8025a0f667e238949fa123d
Maria Pushkareva <dautova.maria92@gmail.com>
2021-01-16 11:43:44 +0100

    add answer NO to name

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,12 @@
 import readlineSync from 'readline-sync';
 
 const askName = () => {
-  const name = readlineSync.question('May I have your name? ');
+  const answer = readlineSync.question('May I have your name? ');
+  let name;
+  if (answer.toLowerCase() === 'no') {
+    name = 'Molly';
+    console.log(`It's a pity! But I will call you ${name}.`);
+  } else { name = answer; }
   console.log(`Hello, ${name}!`);
   return name;
 };
