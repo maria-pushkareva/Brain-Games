9c51ecb862b97c5cb436fffb572069eba7628cfe
Maria Pushkareva <dautova.maria92@gmail.com>
2021-01-18 22:17:14 +0100

    update PROGRESSION game

--- src/games/progression-game.js
+++ src/games/progression-game.js
@@ -3,13 +3,13 @@ import { getRandomNum } from '../utilities.js';
 export const getRandom = () => {
   const firstNum = getRandomNum(1, 100);
   const numDifference = getRandomNum(1, 10);
-
+  const rayLength = getRandomNum(6, 11);
   const ray = [firstNum];
 
-  for (let i = 1; i < 10; i += 1) {
+  for (let i = 1; i < rayLength; i += 1) {
     ray.push(firstNum + (i * numDifference));
   }
-  const missingNumber = getRandomNum(0, 9);
+  const missingNumber = getRandomNum(0, (rayLength - 1));
   return { ray, missingNumber };
 };
 
@@ -17,7 +17,7 @@ export const getQuestion = ({ ray, missingNumber }) => {
   const rayWithHole = [];
   for (let i = 0; i < ray.length; i += 1) {
     if (i === missingNumber) {
-      rayWithHole.push('...');
+      rayWithHole.push('..');
     } else { rayWithHole.push(ray[i]); }
   }
   return rayWithHole.join(' ');
