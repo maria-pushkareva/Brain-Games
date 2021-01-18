9b69b6c6b4eb275ed35689941bc9c2cfeac70083
Maria Pushkareva <dautova.maria92@gmail.com>
2021-01-18 17:46:35 +0100

    add PROGRESSION game

--- README.md
+++ README.md
@@ -6,3 +6,5 @@
 [![asciicast](https://asciinema.org/a/nUQZNfbm01UOq6auk6I4r5T3A.svg)](https://asciinema.org/a/nUQZNfbm01UOq6auk6I4r5T3A)
 
 [![asciicast](https://asciinema.org/a/z1OXcuW9wRVSso4DKsTjSheuq.svg)](https://asciinema.org/a/z1OXcuW9wRVSso4DKsTjSheuq)
+
+[![asciicast](https://asciinema.org/a/P2yucjnqNbw0c4E4zFU39qoC6.svg)](https://asciinema.org/a/P2yucjnqNbw0c4E4zFU39qoC6)
---
+++ src/games/progression-game.js
@@ -0,0 +1,28 @@
+import { getRandomNum } from '../utilities.js';
+
+export const getRandom = () => {
+  const firstNum = getRandomNum(1, 100);
+  const numDifference = getRandomNum(1, 10);
+
+  const ray = [firstNum];
+
+  for (let i = 1; i < 10; i += 1) {
+    ray.push(firstNum + (i * numDifference));
+  }
+  const missingNumber = getRandomNum(0, 9);
+  return { ray, missingNumber };
+};
+
+export const getQuestion = ({ ray, missingNumber }) => {
+  const rayWithHole = [];
+  for (let i = 0; i < ray.length; i += 1) {
+    if (i === missingNumber) {
+      rayWithHole.push('...');
+    } else { rayWithHole.push(ray[i]); }
+  }
+  return rayWithHole.join(' ');
+};
+
+export const getCorrectAnswer = ({ ray, missingNumber }) => ray[missingNumber].toString();
+
+export const description = 'What number is missing in the progression?';
